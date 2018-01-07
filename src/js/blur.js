2cebd8f61d18e667bc42c8582e7c054377546e44
Andrey Ivanov <andrey-tm203@mail.ru>
2018-01-07 09:03:10 +0300

    make some correctings

--- src/js/blur.js
+++
@@ -1,7 +0,0 @@
-const blur = () => {
-  const form = document.querySelector('.contact-form');
-
-  if (!form) return false;
-};
-
-export default blur;
--- src/js/flip.js
+++ src/js/flip.js
@@ -1,39 +1,33 @@
-const box = document.querySelector('.welcome__user-box');
-const auth = document.querySelector('.welcome-header__auth');
-
-const flip = () => {
-  if (!auth) {
-    return false;
-  }
-
-  const user = box.querySelector('.welcome__user');
-  const form = box.querySelector('.welcome__form');
+const flip = userWrapper => {
+  const authButton = document.querySelector('.welcome-header__auth');
+  const user = userWrapper.querySelector('.welcome__user');
+  const form = userWrapper.querySelector('.welcome__form');
   const mainPageButton = form.querySelector('.form__menu-item');
   const maxHeight = Math.max(user.offsetHeight, form.offsetHeight);
 
-  box.style.height = maxHeight + 'px';
+  userWrapper.style.height = maxHeight + 'px';
 
   const authClickHandler = evt => {
     evt.preventDefault();
 
-    box.classList.add('welcome__user-box--rotated');
-    auth.classList.add('welcome-header__auth--hidden');
+    userWrapper.classList.add('welcome__user-box--rotated');
+    authButton.classList.add('welcome-header__auth--hidden');
 
-    auth.removeEventListener('click', authClickHandler);
+    authButton.removeEventListener('click', authClickHandler);
     mainPageButton.addEventListener('click', mainPageButtonClickHandler);
   };
 
   const mainPageButtonClickHandler = evt => {
     evt.preventDefault();
 
-    box.classList.remove('welcome__user-box--rotated');
-    auth.classList.remove('welcome-header__auth--hidden');
+    userWrapper.classList.remove('welcome__user-box--rotated');
+    authButton.classList.remove('welcome-header__auth--hidden');
 
     mainPageButton.removeEventListener('click', mainPageButtonClickHandler);
-    auth.addEventListener('click', authClickHandler);
+    authButton.addEventListener('click', authClickHandler);
   };
 
-  auth.addEventListener('click', authClickHandler);
+  authButton.addEventListener('click', authClickHandler);
   mainPageButton.addEventListener('click', mainPageButtonClickHandler);
 };
 
